7629ebd2b8ebb1597bafe2746ec66992b4fadca0
yinanna <1974618628@qq,com>
2019-01-10 17:53:54 +0800

    lodash

--- lodash/yinanna-lodash.js
+++ lodash/yinanna-lodash.js
@@ -1,12 +1,25 @@
 var yinanna = {
   chunk: function chunk(ary, size = 1) {
-    var result = []
-    for(var i = 0; i< ??; i++) {
-      result.push(ary.slice(i, i + size))
+    var result=[];
+    var n=Math.floor(ary.length/size)+1;
+    var j=0;
+    for(i=0;i<ary.length;i=i+size){
+      var s=ary.slice(i,i+size)
+      result[j]=s;
+      j++;
     }
-    return result
+return result;
   },
   compact: function(ary) {
+    var result=[];
+    for(n=0;n<ary.length;n=n+1){
+      if(ary[n]){
+        result.push(ary[n])
+
+      }
+      return ary;
+    },
+    /*
     var result = []
     for(var item of ary) {
       if (item) {
@@ -15,4 +28,7 @@ var yinanna = {
     }
     return result
   }
+  */
+  concat:function concat(ary,[values])
+
 }
